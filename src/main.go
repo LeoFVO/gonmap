149b6269c859542610975529d33dd31d650908ed
Léo <leodevsecops@gmail.com>
2021-10-10 20:47:01 +0200

    build: concurrent scan

--- src/main.go
+++ src/main.go
@@ -2,18 +2,24 @@ package main
 import(
 	"fmt"
 	"net"
+	"sync"
 )
 
 func main() {
-
+	var wg sync.WaitGroup
 	for i := 1; i <= 1024; i++ {
-		address := fmt.Sprintf("scanme.nmap.org:%d", i)
-		con, err := net.Dial("tcp", address)
-		if err != nil { 
-			// Port closed or filtered
-			continue
-		}
-		con.Close()
-		fmt.Printf("%d open\n", i)
+		wg.Add(1)
+		go func (portNumber int) {
+			defer wg.Done()
+			address := fmt.Sprintf("scanme.nmap.org:%d", portNumber)
+			con, err := net.Dial("tcp", address)
+			if err != nil { 
+				// Port closed or filtered
+				return
+			}
+			con.Close()
+			fmt.Printf("%v open\n", portNumber)
+		}(i)
 	}
+	wg.Wait()
 }
