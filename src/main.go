82eb360e27e2a60135cf828720ab7495d8b83de6
Léo <leodevsecops@gmail.com>
2021-10-10 20:49:50 +0200

    dox: add documention of code

--- src/main.go
+++ src/main.go
@@ -6,11 +6,11 @@ import(
 )
 
 func main() {
-	var wg sync.WaitGroup
+	var wg sync.WaitGroup // Allow use to synchronize main function with go routines
 	for i := 1; i <= 1024; i++ {
-		wg.Add(1)
+		wg.Add(1) // Add 1 routine to group
 		go func (portNumber int) {
-			defer wg.Done()
+			defer wg.Done() // remove 1 routine to group
 			address := fmt.Sprintf("scanme.nmap.org:%d", portNumber)
 			con, err := net.Dial("tcp", address)
 			if err != nil { 
@@ -21,5 +21,5 @@ func main() {
 			fmt.Printf("%v open\n", portNumber)
 		}(i)
 	}
-	wg.Wait()
+	wg.Wait() // wait all routines ended before close.
 }
